68281f6f9e4255fb598776395789d0b2bab110fb
Kyle O'Neill <kyleoneill24@gmail.com>
2018-01-31 19:59:48 -0500

    Converted ExcelCompare into a WPF. Main window done, program not yet in working state

---
+++ ExcelReaderWPF/CS Files/CompareSheets.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelReaderWPF.CS_Files
+{
+    class CompareSheets
+    {
+        public static int Compare(Excel.Worksheet sheet, Excel.Workbook workbook2, Excel.Range writeRange, int writebookRow)
+        {
+            List<String> dictionary = new List<string>()
+            {
+                "CABLE BOX",
+                "FACE-PLATE",
+                "WIRE-MOLD",
+                "CABLE TV",
+                "DATA JACK",
+                "PHONE JACK",
+                "RED PHONE",
+                "CABLE " + "\n" + "BOX "
+            };
+            int writebookColumns = 1;
+            bool written;
+            Excel.Worksheet compareSheet = workbook2.Sheets[sheet.Index];
+            Excel.Range compareRange = compareSheet.UsedRange;
+            Excel.Range range = sheet.UsedRange;
+            int rows = range.Rows.Count;
+            int columns = range.Columns.Count;
+            for (int i = 1; i <= rows; i++)
+            {
+                written = false;
+                float percentage = ((float)i / rows) * 100;
+                //Console.Write(sheet.Name + ": " + percentage.ToString("0.0") + "%");
+                if (range.Cells[i, 4].Value == compareRange.Cells[i, 4].Value && range.Cells[i, 4].Value != null)
+                {
+                    for (int j = 5; j <= 11; j++)
+                    {
+                        if (range.Cells[i, j].Value == compareRange.Cells[i, j].Value && range.Cells[i, j].Value != null)
+                        {
+                            if (dictionary.Contains(Convert.ToString(range.Cells[i, j].Value)) || int.TryParse(Convert.ToString(range.Cells[i, j].Value), out int n))
+                            {
+                                break;
+                            }
+                            writeRange.Cells[writebookRow, 1].Value = sheet.Name;
+                            writeRange.Cells[writebookRow, 2].Value = range.Cells[1, j].Value;
+                            writeRange.Cells[writebookRow, 3].Value = range.Cells[i, 4].Value;
+                            writeRange.Cells[writebookRow, writebookColumns + 3].Value = range.Cells[i, j].Value;
+                            writebookColumns++;
+                            written = true;
+                        }
+                    }
+                    if (written)
+                        writebookRow++;
+                    writebookColumns = 1;
+                }
+            }
+            return writebookRow;
+        }
+    }
+}
---
+++ ExcelReaderWPF/ProgressBar.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExcelReaderWPF
+{
+    /// <summary>
+    /// Interaction logic for ProgressBar.xaml
+    /// </summary>
+    public partial class ProgressBar : Page
+    {
+        public string file1Path { get; set; }
+        public string file2Path { get; set; }
+        public string fileOutPath { get; set; }
+        public int fileOutIndex { get; set; }
+        public ProgressBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
